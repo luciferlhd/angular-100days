ea9568fda57bae2cbea6326ca9f840948bde4006
luciferlhd <lehd4598@gmail.com>
2021-09-09 23:56:12 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  public name1: string = '';
+  user = {
+    name: 'lê huỳnh đức',
+    age: '20',
+    dev: 'c#'
+  };
+  hanrd() {
+    alert('hihihihihihih');
+  }
 }
